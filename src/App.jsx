5431f3e834e0eb30bdc4ad6b4eb8b57d3615e428
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-07-05 20:19:47 -0300

    Subiendo lgo de codigo al repo

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+/*
+  JSX es una extencion de JavaScript. si bien se relaciona con React,js aun asi es pripia de JavaScript.
+  React.js es una combinacion de HTML con JavaScript
+*/
+
+// Cosas que cambian en React.js.
+/**
+  * Cada componente es una funcion.
+    En la parte superior de la funcion vamos a escribir la logica y dentro de return vamos a escribir lka estructura de lo que vamos a mostrar.
+  * Todos los elementos de JSX tienen que ser balanciados, es decir que deben tener una etiqueta de apertura y otra de cierre.
+    una forma de abrebiar esto es con <etiqueta />
+  * En JSX cuando queremos asignarle una clase para despues trabajar con css tenemos que hacer esto "className='nombredelaclase'"
+*/
+
+// Caracteristicas del componente.
+/*
+  Dentro del return vamos a tener un doble parentesis que dentro tiene un elemento que engloba a los demas y si este es eliminado la app se rompe,
+  esto sucede porque cada elemento debe retornar un elemento de JSx.
+
+*/
+
+const App = () => {
+  // Vamos a escribir la logica
+  return (
+    <>
+        {/* Vamos a renderizar la estructura */}
+        <h1> Repasando React.js </h1>
+    </>
+  )
+}
+
+export default App;
